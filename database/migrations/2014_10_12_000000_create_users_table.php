a198c7a03078fa569f9955005ee8b7307fd4976a
AprilyaCInta <aprilya_cinta_27rpl@student.smktelkom-mlg.sch.id>
2020-03-31 19:33:07 +0700

    bismillah

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('firstname');
             $table->string('lastname');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->text('password');
-            $table->text('password_verify');
+            $table->string('password');
+            $table->string('password_verify')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
